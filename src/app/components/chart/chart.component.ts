c6ce33d1cdc152f31cf6a77583c74f625e12c866
Freddy Manrique <fredar21@gmail.com>
2018-02-01 09:51:41 -0500

    add data

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HighchartService } from '../../services/highchart.service'
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-chart',
@@ -9,50 +11,154 @@ import { HighchartService } from '../../services/highchart.service'
 export class ChartComponent implements OnInit {
 
 	//pie = this.chartService.pie;
-  public pie;
-	public line;
-  public data = this.chartService.data();
+  public chart;
 
   constructor( private chartService : HighchartService ) { 
-      this.pieChart(this.data);
-      //this.lineChart(this.data);
-  		
-   }
+    this.pieChart();
+
+  }
 
   ngOnInit() {
-  		setInterval(()=>{
-  			this.pie.addPoint({name: 'Marcos', y: Math.floor(Math.random() * 100)});
-  		}, 3000);
+     //this.chartService.data().subscribe((value)=>console.log(value))
    }
 
-  addPoint() {
-    this.pie.addPoint({name: 'Marcos', y:  Math.floor(Math.random() * 100)});
-  }
- 
-   pieChart( data ){
-    console.log("line");
-    console.log(data);
-
-    let provi = data;
-
-    console.log("provi");
-    console.log(provi);
-    provi.options.chart.type = "pie";
-    this.pie = data;
-    
+   addPoint() {
+    //this.pie.addPoint({name: 'Marcos', y:  Math.floor(Math.random() * 100)});
   }
 
-   lineChart( data ){
-    console.log("line");
-    console.log(data);
+  pieChart( ){
 
-    let provi = data;
+    let pie= new Chart({
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Produccion De Juguetes'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+        }
+      },
+      series: [{
+        name: 'Brands',
+        data: []
+      }]
+    });
 
-    console.log("provi");
-    console.log(provi);
-    provi.options.chart.type = "line";
-    this.line = data;
-    
+    let data=[];
+
+    this.chartService.data().subscribe((value)=>{
+      let a= value.length;
+      let obj;
+
+      for(let i=0; i<a; i++){
+        let name = value[i].name;
+        let cant = value[i].cant;
+        obj = {name:name, y:cant};
+        data.push(obj);
+        this.chart.addPoint(obj);
+      }
+      
+      data = [];
+    });
+
+
+    this.chart = pie;
+  }
+
+
+  lineChart(){
+    let line= new Chart({
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Browser market shares January, 2015 to May, 2015'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+        }
+      },
+      series: [{
+        name: 'Brands',
+        data: []
+      }]
+    });
+
+
+    let data=[];
+
+    this.chartService.data().subscribe((value)=>{
+
+
+      let a= value.length;
+      console.log(line);
+      let obj;
+
+      for(let i=0; i<a; i++){
+        let time = value[i].time;
+        let timeToy = value[i].timeByToy;
+        obj = {name:timeToy, y:time};
+        data.push(obj);
+      }
+
+      this.chart.addPoint(data);
+        //pie.options.series[0].data = data;
+        console.log(data);
+        data = [];
+
+      });
+
+
+    let array = [{
+      name: 'IE',
+      y: 56.33
+    }, {
+      name: 'Chrome',
+      y: 24.03,
+      sliced: true,
+      selected: true
+    }, {
+      name: 'Firefox',
+      y: 10.38
+    }, {
+      name: 'Safari',
+      y: 4.77
+    }, {
+      name: 'Opera',
+      y: 0.91
+    }, {
+      name: 'Other',
+      y: 0.2
+    }] 
+
+    line.options.series[0].data = array;
+
+
   }
 
 }
--- src/app/services/highchart.service.ts
+++ src/app/services/highchart.service.ts
@@ -1,121 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Chart } from 'angular-highcharts';
+import {Observable} from 'rxjs';
+
+
 
 @Injectable()
 export class HighchartService {
 
 
-	chart = new Chart({
-		chart: {
-			plotBackgroundColor: null,
-			plotBorderWidth: null,
-			plotShadow: false,
-			type: ''
-		},
-		title: {
-			text: 'Browser market shares January, 2015 to May, 2015'
-		},
-		tooltip: {
-			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-		},
-		plotOptions: {
-			pie: {
-				allowPointSelect: true,
-				cursor: 'pointer',
-				dataLabels: {
-					enabled: true,
-					format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-				}
-			}
-		},
-		series: [{
-			name: 'Brands',
-			data: [{
-				name: 'IE',
-				y: 56.33
-			}, {
-				name: 'Chrome',
-				y: 24.03,
-				sliced: true,
-				selected: true
-			}, {
-				name: 'Firefox',
-				y: 10.38
-			}, {
-				name: 'Safari',
-				y: 4.77
-			}, {
-				name: 'Opera',
-				y: 0.91
-			}, {
-				name: 'Other',
-				y: 0.2
-			}]
-		}]
+	basicData = [ //datos que representan la cantidad y tiempo de produccion de juguetes por empleado
+	{name:"Marcos", cant: 15, timeByToy:4, time: 1517407236669},
+	{name:"Fabian", cant: 62, timeByToy:1, time: 1517407236669},
+	{name:"Andres", cant: 23, timeByToy:2, time: 1517407236669},
+	{name:"Melissa", cant: 45, timeByToy:3, time: 1517407236669}
+	];
+
+
+	constructor() {
 	}
-	);
 
+	data():Observable<any[]>{
+		return new Observable<any[]>((observer)=>{
+			setInterval(()=>{
+				const time = Date.now();
+				const newData = this.basicData.map(item=>{
+					return {
+						name: item.name,
+						cant : Math.floor(Math.random()*100),
+						timeByToy : Math.floor(Math.random()*10),
+						time
+					};	
+				});
 
-	line = new Chart({
-		chart: {
-			plotBackgroundColor: null,
-			plotBorderWidth: null,
-			plotShadow: false,
-			type: 'line'
-		},
-		title: {
-			text: 'Browser market shares January, 2015 to May, 2017'
-		},
-		tooltip: {
-			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-		},
-		plotOptions: {
-			pie: {
-				allowPointSelect: true,
-				cursor: 'pointer',
-				dataLabels: {
-					enabled: true,
-					format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-				}
-			}
-		},
-		series: [{
-			name: 'Brands',
-			data: [{
-				name: 'IE',
-				y: 56.33
-			}, {
-				name: 'Chrome',
-				y: 24.03,
-				sliced: true,
-				selected: true
-			}, {
-				name: 'Firefox',
-				y: 10.38
-			}, {
-				name: 'Safari',
-				y: 4.77
-			}, {
-				name: 'Opera',
-				y: 0.91
-			}, {
-				name: 'Other',
-				y: 0.2
-			}]
-		}]
+				observer.next(newData);
+			}, 10000);
+		});
 	}
-	);
 
-
-
-  constructor() {
-  	console.log("servicio funcionando");
-  }
-
-   data(){
-   	return this.chart;
-   }
-  
 
 }
 
