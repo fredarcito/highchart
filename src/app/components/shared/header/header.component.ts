7fdc69c739b1b1f35363c4a14331b482efb71820
Freddy Manrique <fredar21@gmail.com>
2018-02-05 08:54:19 -0500

    Intercambio Componentes

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+	@Output() dato: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle(){
+  	console.log("click");
+  	this.dato.emit("emitiendo dato desde el header");
   }
 
 }
